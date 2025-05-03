0e05e3deee67c1ed726dcaa0b79bd42473443513
Joanna Kliś <joannnaklis8@gmail.com>
2025-05-03 12:39:25 +0200

    implementacja pętli gry

--- GamePanel.java
+++ GamePanel.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Objects;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
 
     Image backgroundImg, flappyBirdImg, topPipeImg, bottomPipeImg;
 
     Bird bird;
+
+    Timer gameLoop;
 
     GamePanel() {
         setPreferredSize(new Dimension(GameConstants.BOARD_WIDTH, GameConstants.BOARD_HEIGHT));
@@ -17,6 +21,9 @@ public class GamePanel extends JPanel {
         bottomPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./bottompipe.png"))).getImage();
 
         bird = new Bird(flappyBirdImg, GameConstants.BOARD_WIDTH/8, GameConstants.BOARD_HEIGHT/2);
+
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
     }
 
     public void paintComponent(Graphics g) {
@@ -25,8 +32,14 @@ public class GamePanel extends JPanel {
     }
 
     public void draw(Graphics g) {
+        System.out.println("test");
         g.drawImage(backgroundImg, 0, 0, GameConstants.BOARD_WIDTH, GameConstants.BOARD_HEIGHT, null);
 
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
     }
 }
